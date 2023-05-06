a61b74c608ffd302c1c439797f564d106c66361f
Sweetbread <glebzaharov1@gmail.com>
2023-05-07 01:55:55 +0300

    [fix]: Fix paths

--- cogs/stickers.py
+++ cogs/stickers.py
@@ -4,7 +4,9 @@ from discord                import app_commands
 from discord.app_commands   import Choice
 from discord.ext            import commands, tasks
 from os                     import listdir
-from os.path                import splitext
+from os.path                import splitext, join
+
+dir = "Stickers"
 
 class Stickers(commands.Cog, name="Стикеры"):
     def __init__(self, bot):
@@ -20,7 +22,7 @@ class Stickers(commands.Cog, name="Стикеры"):
             color = discord.Color(0xaaffaa)
 
         
-        list_ = listdir("/home/pi/Koteika/Stickers")
+        list_ = listdir(dir)
         embed = discord.Embed(title="Стикеры", description="\n".join([f"{i+1}: {list_[i]}" for i in range(len(list_))]), color=color)
         await ctx.send(embed=embed)
 
@@ -35,7 +37,7 @@ class Stickers(commands.Cog, name="Стикеры"):
 
         embed = discord.Embed(color=color)
         embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar)
-        with open(f"/home/pi/Koteika/Stickers/{listdir('/home/pi/Koteika/Stickers')[sticker-1]}", 'rb') as f:
+        with open(join(dir, listdir(dir)[sticker-1]), 'rb') as f:
             await ctx.send(content, file=discord.File(f), embed=embed, reference=ctx.message.reference)
             try: await ctx.message.delete()
             except: pass
@@ -43,9 +45,9 @@ class Stickers(commands.Cog, name="Стикеры"):
     @app_commands.command(name="sticker", description="Отправляет стикер")
     @app_commands.guilds(discord.Object(822157545622863902))
     @app_commands.describe(sticker="Стикер")
-    @app_commands.choices(sticker=[Choice(name=splitext(i)[0], value=i) for i in listdir("/home/pi/Koteika/Stickers")])
+    @app_commands.choices(sticker=[Choice(name=splitext(i)[0], value=i) for i in listdir(dir)])
     async def send_sticker(self, inter, sticker: Choice[str]):
-        with open(f"/home/pi/Koteika/Stickers/{sticker.value}", 'rb') as f:
+        with open(join(dir, sticker.value), 'rb') as f:
             await inter.response.send_message(file=discord.File(f))
     
 
