1bcce1915305ac4e9b2f9b7521939028b9355b73
Sweetbread <glebzaharov1@gmail.com>
2023-05-16 20:36:33 +0300

    [refactor]: Moved demotivators to fun

--- cogs/fun.py
+++ cogs/fun.py
@@ -58,5 +58,25 @@ class Fun(commands.Cog):
 
         remove(join(tmp_dir, filename))
 
+
+    @app_commands.command()
+    async def demotivator(self, inter: discord.Interaction, title: str, text: str, image: discord.Attachment):
+        logger.debug((title, text))
+
+        if not "image" in image.content_type:
+            await inter.response.send_message("Это не изображение")
+            return
+
+        logger.debug("Meow")
+        filename = join("tmp", f"{inter.id}_{image.filename}")
+        logger.debug(filename)
+        await image.save(filename)
+        Dem(title, text).create(filename, font_name="FreeSans.ttf")
+        with open('demresult.jpg', 'rb') as f:
+            await inter.response.send_message(file=discord.File(f))
+        remove(filename)
+        remove('demresult.jpg')
+
+
 async def setup(bot): 
     await bot.add_cog(Fun())
